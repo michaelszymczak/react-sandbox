ac17886a6e16b161ced21ce2431f4bfacc20a082
Michael Szymczak <contact@michaelszymczak.com>
2019-11-27 22:28:36 +0000

    change homepage url

---
+++ docs/precache-manifest.4c0e880b06b675accc64919ba3184973.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd285e86aec33a7b9070ca88b92bfa45",
+    "url": "/react-sandbox/index.html"
+  },
+  {
+    "revision": "a93188f1ab0f67a4a950",
+    "url": "/react-sandbox/static/css/main.3b491531.chunk.css"
+  },
+  {
+    "revision": "a5a833057fbfee5ad223",
+    "url": "/react-sandbox/static/js/2.5cd19e46.chunk.js"
+  },
+  {
+    "revision": "a93188f1ab0f67a4a950",
+    "url": "/react-sandbox/static/js/main.e5495ed3.chunk.js"
+  },
+  {
+    "revision": "d52d11df4fc315604c3c",
+    "url": "/react-sandbox/static/js/runtime-main.4937af57.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe1801cea88f759fe6dc6fbb01bccba0.js"
+  "/react-sandbox/precache-manifest.4c0e880b06b675accc64919ba3184973.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-sandbox/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
